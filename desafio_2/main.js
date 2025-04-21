b3b960714601d4bb408ba00b94bef712e6f48f8b
sofi <sofiaferboix@gmail.com>
2025-04-21 20:42:41 -0300

    first commit

---
+++ desafio_2/main.js
@@ -0,0 +1,5 @@
+let nombreProducto = "coca-cola";
+const precioUnitario = 1500;
+const cantidadDeseada = prompt (`¿Cuántas ${nombreProducto} que cuesta $${precioUnitario} cada una desea comprar?`)
+let costoTotal = precioUnitario * cantidadDeseada
+alert (`El costo total de su compra es: $${costoTotal}`)
