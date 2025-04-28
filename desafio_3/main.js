cf47598e0238e8fbce66a2982d1bdbe043f1378d
sofi <sofiaferboix@gmail.com>
2025-04-28 18:35:35 -0300

    desafio 3-act3

--- desafio_3/main.js
+++ desafio_3/main.js
@@ -1,6 +1,7 @@
 let nombreProducto = "coca-cola";
 let precioUnitario = 1500;
 let cantidadDeseada = prompt (`¿Cuántas ${nombreProducto} que cuesta $${precioUnitario} cada una desea comprar? Si elige 5 o más unidades tendrá un descuento del 10%`)
+cantidadDeseada = parseInt (cantidadDeseada)
 const costoTotal = precioUnitario * cantidadDeseada
 if (cantidadDeseada >= 5) {
     let costoConDescuento = costoTotal - costoTotal*10/100
