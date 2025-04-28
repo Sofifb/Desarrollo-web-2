5ff4561a8dd83835e2aa20b8a380d41275d27249
sofi <sofiaferboix@gmail.com>
2025-04-28 18:29:57 -0300

    desafio 3-act2

--- desafio_3/main.js
+++ desafio_3/main.js
@@ -3,8 +3,7 @@ let precioUnitario = 1500;
 let cantidadDeseada = prompt (`¿Cuántas ${nombreProducto} que cuesta $${precioUnitario} cada una desea comprar? Si elige 5 o más unidades tendrá un descuento del 10%`)
 const costoTotal = precioUnitario * cantidadDeseada
 if (cantidadDeseada >= 5) {
-    let descuento = costoTotal*10/100
-    let costoConDescuento = costoTotal - descuento
+    let costoConDescuento = costoTotal - costoTotal*10/100
     alert (`El costo total de su compra con un 10% de descuento es: $${costoConDescuento}`)
 }else{
     alert (`El costo total de su compra es: $${costoTotal}`)
